c368034ff41bbd6581475c28d4ad4756b8c977b2
blueforeverabv <blueforeverabv@gmail.com>
2012-03-14 18:58:24 +0000

    Some minor changes and tweaks. This is the Olympiad version.

--- NetworkMonitorApplication/Properties/AssemblyInfo.cs
+++ NetworkMonitorApplication/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("NetworkMonitorApplication")]
+[assembly: AssemblyTitle("Мрежов Анализатор")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("NetworkMonitorApplication")]
+[assembly: AssemblyCompany("PMG Soft")]
+[assembly: AssemblyProduct("Мрежов Анализатор")]
 [assembly: AssemblyCopyright("Copyright ©  2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
